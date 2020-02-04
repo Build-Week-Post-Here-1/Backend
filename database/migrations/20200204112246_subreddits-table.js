c20795cc1f78780fec0722f2216be0bdc5a1e680
Christine Fletcher <chrismis79@gmail.com>
2020-02-04 15:00:50 -0600

    added subLink column to subreddit table

--- database/migrations/20200204112246_subreddits-table.js
+++ database/migrations/20200204112246_subreddits-table.js
@@ -3,8 +3,9 @@ exports.up = function(knex) {
   return knex.schema 
     .createTable('subreddits', function(tbl) {
         tbl.increments();
-        tbl.string('name')
+        tbl.string('name', 128)
             .notNullable();
+        tbl.string('subLink', 128);
         tbl
             .integer('user_id')
             .unsigned()
